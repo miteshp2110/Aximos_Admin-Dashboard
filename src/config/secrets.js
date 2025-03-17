d97851400397c1d52cadda2a76bafc5fe5705045
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-03-17 10:16:57 +0530

    Added login

--- src/config/secrets.js
+++ src/config/secrets.js
@@ -9,4 +9,6 @@ module.exports = {
     MYSQL_PASSWORD : process.env.MYSQL_PASSWORD,
     MYSQL_LIMIT : process.env.MYSQL_LIMIT,
     MYSQL_DATABASE : process.env.MYSQL_DATABASE,
+    JWT_SECRET : process.env.JWT_SECRET,
+    BCRYPT_SALT : process.env.BCRYPT_SALT
 }
