71c615e095523b0487e2deb1f3f99d425d3b6baf
miteshp2110 <paliwalmitesh2110@gmail.com>
2025-03-17 13:16:16 +0530

    Added admin signup

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -1,8 +1,11 @@
+const adminSignUp = require("../controllers/auth-controller/adminSignup");
 const login = require("../controllers/auth-controller/login");
-const express = require('express')
+const express = require('express');
+const checkAdmin = require("../middlewares/checkAdmin");
 const router = express.Router()
 
 router.post('/login',login)
+router.post('/signup',checkAdmin,adminSignUp) // protected route
 
 
 module.exports = router
